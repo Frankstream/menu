8d43f06f06e573d4a01213fa8e74c61b9d31d049
Frankstream <56765146+Frankstream@users.noreply.github.com>
2021-12-03 07:44:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional jQuery class toggle for added robustness. 
+
+/* 
+var labelID;
+
+$('.nav-mobile label').click(function() {
+  labelID = $(this).attr('for');
+  $('#' + labelID).toggleClass('active');
+}); 
+*/
